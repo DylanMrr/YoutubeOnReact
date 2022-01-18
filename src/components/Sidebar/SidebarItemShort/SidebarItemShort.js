8260c12fc7891f98d0103922086d51e9bd177b97
OSINIT\dmitriy.eginov <dmitriy.eginov@osinit.com>
2022-01-18 15:20:30 +0300

    items fix padding

--- src/components/Sidebar/SidebarItemShort/SidebarItemShort.js
+++ src/components/Sidebar/SidebarItemShort/SidebarItemShort.js
@@ -6,7 +6,7 @@ export default ({selected, Icon, title}) => {
     return(
         <div className={`sidebarItemShort ${selected && 'selected'}`}>
             <Icon className="sidebarItemShort__icon"/>
-            <h2 className="sidebarItemShort__title">{title}</h2>
+            <div className="sidebarItemShort__title">{title}</div>
         </div>
     )
 }
