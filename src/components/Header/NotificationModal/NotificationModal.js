7e1fc0020836032da83cc163991e20dbb23933a8
dmitriyeginov <DylanMr@yandex.ru>
2022-01-18 22:01:25 +0300

    Окно уведомлений - верстка

--- src/components/Header/NotificationModal/NotificationModal.js
+++ src/components/Header/NotificationModal/NotificationModal.js
@@ -1,6 +1,8 @@
 import React from "react";
 import SettingsIcon from '@mui/icons-material/Settings';
 
+import NotificationModalItem from "./NotificationModalItem/NotificationModalItem";
+
 import './NotificationModal.css'
 
 export default () => {
@@ -15,6 +17,46 @@ export default () => {
                 </div>
             </div>
 
+            <hr />
+
+            <div className="notificationModal__items">
+                <NotificationModalItem 
+                    avatar="https://yt3.ggpht.com/ytc/AKedOLR87l_OkveAWz6c26dKlxtmx3k55i6I6mFUm668=s176-c-k-c0x00ffffff-no-rj"
+                    text="Всем привет! Сегодня  заключительная часть Подбор авто за 150.  У Славы еще есть шанс выйти на равных если вы проголосуете за его автомобиль!  Смотрите видео и участвуйте в розыгрыше авто, который вы сами выбираете! "
+                    date="2 дня назад"
+                    preview="https://i.ytimg.com/vi/pnyVKNlzVOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD50NNpHZ5wWGkllOVGpHfkVCns2g"
+                />
+                <NotificationModalItem 
+                    avatar="https://yt3.ggpht.com/ytc/AKedOLR87l_OkveAWz6c26dKlxtmx3k55i6I6mFUm668=s176-c-k-c0x00ffffff-no-rj"
+                    text="Всем привет! Сегодня  заключительная часть Подбор авто за 150.  У Славы еще есть шанс выйти на равных если вы проголосуете за его автомобиль!  Смотрите видео и участвуйте в розыгрыше авто, который вы сами выбираете! "
+                    date="2 дня назад"
+                    preview="https://i.ytimg.com/vi/pnyVKNlzVOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD50NNpHZ5wWGkllOVGpHfkVCns2g"
+                />
+                <NotificationModalItem 
+                    avatar="https://yt3.ggpht.com/ytc/AKedOLR87l_OkveAWz6c26dKlxtmx3k55i6I6mFUm668=s176-c-k-c0x00ffffff-no-rj"
+                    text="Всем привет! Сегодня  заключительная часть Подбор авто за 150.  У Славы еще есть шанс выйти на равных если вы проголосуете за его автомобиль!  Смотрите видео и участвуйте в розыгрыше авто, который вы сами выбираете! "
+                    date="2 дня назад"
+                    preview="https://i.ytimg.com/vi/pnyVKNlzVOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD50NNpHZ5wWGkllOVGpHfkVCns2g"
+                />
+                <NotificationModalItem 
+                    avatar="https://yt3.ggpht.com/ytc/AKedOLR87l_OkveAWz6c26dKlxtmx3k55i6I6mFUm668=s176-c-k-c0x00ffffff-no-rj"
+                    text="Всем привет! Сегодня  заключительная часть Подбор авто за 150.  У Славы еще есть шанс выйти на равных если вы проголосуете за его автомобиль!  Смотрите видео и участвуйте в розыгрыше авто, который вы сами выбираете! "
+                    date="2 дня назад"
+                    preview="https://i.ytimg.com/vi/pnyVKNlzVOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD50NNpHZ5wWGkllOVGpHfkVCns2g"
+                />
+                <NotificationModalItem 
+                    avatar="https://yt3.ggpht.com/ytc/AKedOLR87l_OkveAWz6c26dKlxtmx3k55i6I6mFUm668=s176-c-k-c0x00ffffff-no-rj"
+                    text="Всем привет! Сегодня  заключительная часть Подбор авто за 150.  У Славы еще есть шанс выйти на равных если вы проголосуете за его автомобиль!  Смотрите видео и участвуйте в розыгрыше авто, который вы сами выбираете! "
+                    date="2 дня назад"
+                    preview="https://i.ytimg.com/vi/pnyVKNlzVOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD50NNpHZ5wWGkllOVGpHfkVCns2g"
+                />
+                <NotificationModalItem 
+                    avatar="https://yt3.ggpht.com/ytc/AKedOLR87l_OkveAWz6c26dKlxtmx3k55i6I6mFUm668=s176-c-k-c0x00ffffff-no-rj"
+                    text="Всем привет! Сегодня  заключительная часть Подбор авто за 150.  У Славы еще есть шанс выйти на равных если вы проголосуете за его автомобиль!  Смотрите видео и участвуйте в розыгрыше авто, который вы сами выбираете! "
+                    date="2 дня назад"
+                    preview="https://i.ytimg.com/vi/pnyVKNlzVOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD50NNpHZ5wWGkllOVGpHfkVCns2g"
+                />
+            </div>
             
         </div>
     )
--- src/components/Header/NotificationModal/NotificationModalItem/NotificationModalItem.js
+++ src/components/Header/NotificationModal/NotificationModalItem/NotificationModalItem.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+import { Avatar } from "@mui/material";
+
 import './NotificationModalItem.css'
 
-export default () => {
+export default ({avatar, text, date, preview}) => {
     return (
-        <div>
-            
+        <div className="notificationModalItem">
+            <Avatar 
+                src={avatar}
+                
+            />
+            <div>
+                <div className="notificationModalItem__text">
+                    {text}
+                </div>
+                <div className="notificationModalItem__date">
+                    {date}
+                </div>
+            </div>
+            <img 
+                className="notificationModalItem__preview"
+                src={preview}
+            />
         </div>
     )
 }
