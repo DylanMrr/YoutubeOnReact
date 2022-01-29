bca6737dd7137391f888bce89e335fdbf9495e8e
dmitriyeginov <DylanMr@yandex.ru>
2022-01-29 16:43:57 +0300

    Смена brouserRoute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import  Header from './components/Header/Header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import SearchPage from './pages/SearchPage/SearchPage';
 import HomePage from './pages/HomePage/HomePage'
 import NavigatorPage from './pages/NavigatorPage/NavigatorPage';
@@ -11,7 +11,7 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 function App() {
   return (
     <div className='app'>
-      <BrowserRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Header />
 
         <Routes>
@@ -22,7 +22,7 @@ function App() {
 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
